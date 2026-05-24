a3f3893df9fa44eedc29edbf57066cb780e8e9e6
Aldo Didi <201050222@aguascalientes.tecnm.mx>
2026-05-24 15:55:20 -0600

    lógica de cálculo de envío

--- app/Http/Controllers/EnvioController.php
+++ app/Http/Controllers/EnvioController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class EnvioController extends Controller
 {
-    //
+    public function calcularEnvio($peso, $distancia, $prioridad)
+    {
+        if ($peso <= 0 || $distancia <= 0) {
+            return "Datos inválidos";
+        }
+
+        $costo = 0;
+
+        if ($peso <= 1) {
+            $costo = 80;
+        } elseif ($peso <= 5) {
+            $costo = 150;
+        } else {
+            $costo = 250;
+        }
+
+        if ($distancia > 50) {
+            $costo += 50;
+        }
+
+        if ($prioridad === "express") {
+            $costo += 100;
+        }
+
+        return $costo;
+    }
 }
