034fcd664f247468a75d7de6ac63c0b7c84c20c1
Aldo Didi <201050222@aguascalientes.tecnm.mx>
2026-05-24 17:33:06 -0600

    cambio

--- app/Http/Controllers/ConversorController.php
+++ app/Http/Controllers/ConversorController.php
@@ -1,39 +1,21 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class ConversorController extends Controller
+public function convertir($cantidad, $moneda)
 {
-    public function convertir(Request $request)
-    {
-        $cantidad = $request->cantidad;
-        $de = $request->de;
-        $a = $request->a;
+    $tasas = [
+        'usd' => 18.5,
+        'mxn' => 0.054,
+    ];
 
-        $tasas = [
-            'USD_MXN' => 18.5,
-            'MXN_USD' => 0.054,
-            'EUR_MXN' => 21,
-            'MXN_EUR' => 0.047,
-            'USD_EUR' => 0.92,
-            'EUR_USD' => 1.08,
-        ];
-
-        $clave = $de . '_' . $a;
-
-        if (!isset($tasas[$clave])) {
-            return back()->with('error', 'Conversión no soportada');
-        }
-
-        $resultado = $cantidad * $tasas[$clave];
-
-        return view('conversor', [
-            'resultado' => $resultado,
-            'cantidad' => $cantidad,
-            'de' => $de,
-            'a' => $a
-        ]);
+    if (!isset($tasas[$moneda])) {
+        return response()->json([
+            'error' => 'Moneda inválida'
+        ], 400);
     }
+
+    $resultado = $cantidad * $tasas[$moneda];
+
+    return response()->json([
+        'cantidad' => $cantidad,
+        'moneda' => $moneda,
+        'resultado' => $resultado
+    ], 200);
 }
