27c5942110909d778c3e018241271fab7313c78d
Aldo Didi <201050222@aguascalientes.tecnm.mx>
2026-05-24 15:57:14 -0600

    Codigo de pruebas de envío express

--- tests/Feature/EnvioTest.php
+++ tests/Feature/EnvioTest.php
@@ -2,19 +2,44 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Http\Controllers\EnvioController;
 
 class EnvioTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function test_envio_normal()
     {
-        $response = $this->get('/');
+        $envio = new EnvioController();
 
-        $response->assertStatus(200);
+        $resultado = $envio->calcularEnvio(2, 20, "normal");
+
+        $this->assertEquals(150, $resultado);
+    }
+
+    public function test_envio_express()
+    {
+        $envio = new EnvioController();
+
+        $resultado = $envio->calcularEnvio(2, 20, "express");
+
+        $this->assertEquals(250, $resultado);
+    }
+
+    public function test_envio_distancia_larga()
+    {
+        $envio = new EnvioController();
+
+        $resultado = $envio->calcularEnvio(6, 100, "normal");
+
+        $this->assertEquals(300, $resultado);
+    }
+
+    public function test_datos_invalidos()
+    {
+        $envio = new EnvioController();
+
+        $resultado = $envio->calcularEnvio(-1, 20, "normal");
+
+        $this->assertEquals("Datos inválidos", $resultado);
     }
 }
